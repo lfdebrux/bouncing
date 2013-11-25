2bd0eb0fa573d58577cb778402c9b485bcbd906a
Laurence de Bruxelles <lfdebrux@gmail.com>
2013-11-25 23:14:32 +0000

    added benchmark for Jump

---
+++ jump_test.go
@@ -0,0 +1,14 @@
+package bouncing
+
+import "testing"
+
+func BenchmarkJump(b *testing.B) {
+	InitMaxwellian()
+	defer FreeMaxwellian()
+	
+	p := RandParticle(Water)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Jump()
+	}
+}
