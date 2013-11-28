821453450989c51d8d3dad0719339946beae7360
Laurence de Bruxelles <lfdebrux@gmail.com>
2013-11-28 14:20:51 +0000

    make cridervondrak2000 expression for flighttime default for Jump

--- jump.go
+++ jump.go
@@ -11,7 +11,7 @@ func (p *P) Jump() (*J,*Lost) {
 	j.V = RandVelocity(Mass[j.Type],j.Phi)
 	j.Psi,j.ThetaDash = ButlerRandDirection()
 	j.Phi,j.Beta = PositionJump(j.Phi,j.Beta,j.V,j.Psi,j.ThetaDash)
-	j.T = ButlerFlightTime(j.V,j.ThetaDash)
+	j.T = VondrakFlightTime(j.V,j.ThetaDash)
 
 	if lost := j.IsLost(); lost != nil {
 		p = RandParticle(p.Type)
