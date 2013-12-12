396d9b9fd020e5b60256a046104c8b1032ef80d8
Laurence de Bruxelles <lfdebrux@gmail.com>
2013-12-12 13:12:48 +0000

    fixed temperature tests

--- temperature.go
+++ temperature.go
@@ -4,8 +4,13 @@ import "math"
 
 type TemperatureFunc func(float64) float64
 
+const (
+	ButlerT0 = 151.0
+	ButlerT1 = 161.7
+)
+
 func ButlerTemperature(phi float64) float64 {
-	return 151 + 161.7*math.Pow(math.Cos(phi-math.Pi/2),0.59)
+	return ButlerT0 + ButlerT1*math.Pow(math.Cos(phi-math.Pi/2),0.59)
 }
 
 func VondrakTemperature(solarzenith float64) float64 {
--- temperature_test.go
+++ temperature_test.go
@@ -7,46 +7,46 @@ import "math/rand"
 
 func BenchmarkTemperature(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Temperature(rand.Float64()*math.Pi)
+		ButlerTemperature(rand.Float64()*math.Pi)
 	}
 }
 
 func TestTemperaturePoleLimit(t *testing.T) {
-	if T := Temperature(0); math.Abs(T - T0) > TOL {
-		t.Log(math.Abs(T - T0))
-		t.Errorf("Temperature at Lunar Pole should be T0=%f, instead got %f",T0,T)
+	if T := ButlerTemperature(0); math.Abs(T - ButlerT0) > TOL {
+		t.Log(math.Abs(T - ButlerT0))
+		t.Errorf("Temperature at Lunar Pole should be ButlerT0=%f, instead got %f",ButlerT0,T)
 	}
 }
 
 func TestTemperatureEquatorLimit(t *testing.T) {
-	Te := T0 + T1
-	if T := Temperature(math.Pi/2); math.Abs(T - Te) > TOL {
+	Te := ButlerT0 + ButlerT1
+	if T := ButlerTemperature(math.Pi/2); math.Abs(T - Te) > TOL {
 		t.Log(math.Abs(T - Te))
-		t.Errorf("Temperature at Lunar equator should be T0+T1=%f, instead got %f",Te,T)
+		t.Errorf("Temperature at Lunar equator should be ButlerT0+ButlerT1=%f, instead got %f",Te,T)
 	}
 }
 
-func TestTemperatureT0IsMin(t *testing.T) {
+func TestTemperatureButlerT0IsMin(t *testing.T) {
 	for phi := 0.0; phi < math.Pi/2; phi += 0.1 {
-		if T := Temperature(phi); T < T0 {
-			t.Errorf("Temperature should never be lower than T0=%f, but at phi %f got %f",T0,phi,T)
+		if T := ButlerTemperature(phi); T < ButlerT0 {
+			t.Errorf("Temperature should never be lower than ButlerT0=%f, but at phi %f got %f",ButlerT0,phi,T)
 		}
 	}
 }
 
 func TestTemperatureTeIsMax(t *testing.T) {
-	Te := T0 + T1
+	Te := ButlerT0 + ButlerT1
 	for phi := 0.0; phi < math.Pi/2; phi += 0.1 {
-		if T := Temperature(phi); T > Te {
+		if T := ButlerTemperature(phi); T > Te {
 			t.Errorf("Temperature should never be higher than Te=%f, but at phi %f got %f",Te,phi,T)
 		}
 	}
 }
 
 func TestTemperatureMonotonic(t *testing.T) {
-	Tlast := Temperature(0)
+	Tlast := ButlerTemperature(0)
 	for phi := 0.0; phi < math.Pi/2; phi += 0.1 {
-		if T := Temperature(phi); T < Tlast {
+		if T := ButlerTemperature(phi); T < Tlast {
 			t.Errorf("Temperature should vary monotonically, but it didn't")
 		}
 	}
@@ -54,9 +54,9 @@ func TestTemperatureMonotonic(t *testing.T) {
 
 func TestTemperatureSymmetric(t *testing.T) {
 	for phi := 0.0; phi < math.Pi/2; phi += 0.1 {
-		if math.Abs(Temperature(phi) - Temperature(math.Pi-phi)) > TOL {
-			t.Log(Temperature(phi) - Temperature(math.Pi-phi))
-			t.Errorf("Temperature function should be symmetric, but Temperature(%f)=%f!=Temperature(%f)=%f",phi,Temperature(phi),math.Pi-phi,Temperature(math.Pi-phi))
+		if math.Abs(ButlerTemperature(phi) - ButlerTemperature(math.Pi-phi)) > TOL {
+			t.Log(ButlerTemperature(phi) - ButlerTemperature(math.Pi-phi))
+			t.Errorf("Temperature function should be symmetric, but Temperature(%f)=%f!=Temperature(%f)=%f",phi,ButlerTemperature(phi),math.Pi-phi,ButlerTemperature(math.Pi-phi))
 		}
 	}
 }
