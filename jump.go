2adc30fbb8a88eb469d0fcc6838c108fcc4a5c96
Laurence de Bruxelles <lfdebrux@gmail.com>
2014-02-01 11:50:13 +0000

    added JumpMethod type

--- jump.go
+++ jump.go
@@ -7,6 +7,7 @@ type J struct {
 	T float64
 }
 
+type JumpMethod func(*J)
 type JumpFunc func(*P) (*J,*Lost)
 
 func Jump(p *P) (*J,*Lost) {
@@ -15,7 +16,7 @@ func Jump(p *P) (*J,*Lost) {
 	j.V = RandVelocity(Mass[j.Type],j.Temperature)
 	j.Psi,j.ThetaDash = RandDirection()
 	j.Phi,j.Beta = ButlerPositionJump(j.Phi,j.Beta,j.V,j.Psi,j.ThetaDash)
-	j.T = FlightTime(j.V,j.ThetaDash)
+	FlightTime(j)
 
 	j.Time += j.T
 
@@ -27,14 +28,14 @@ func Jump(p *P) (*J,*Lost) {
 	return j,nil
 }
 
-func NewJump(tm TemperatureFunc,rd RandDirectionFunc,pj PositionJumpFunc,ft FlightTimeFunc) JumpFunc {
+func NewJump(tm TemperatureFunc,rd RandDirectionFunc,pj PositionJumpFunc,ft JumpMethod) JumpFunc {
 	return func(p *P) (*J,*Lost) {
 		j := &J{P:p}
 		j.Temperature = tm(j.Phi,j.Beta)
 		j.V = RandVelocity(Mass[j.Type],j.Temperature)
 		j.Psi,j.ThetaDash = rd()
 		j.Phi,j.Beta = pj(j.Phi,j.Beta,j.V,j.Psi,j.ThetaDash)
-		j.T = ft(j.V,j.ThetaDash)
+		ft(j)
 
 		j.Time += j.T
 
