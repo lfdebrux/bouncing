29f46b2f805f63aea0a615a0ac76eae449c3db96
Laurence de Bruxelles <lfdebrux@gmail.com>
2014-03-03 19:05:51 +0000

    added Jump tests

--- jump_test.go
+++ jump_test.go
@@ -2,6 +2,24 @@ package bouncing
 
 import "testing"
 
+func TestJump(t *testing.T) {
+	InitMaxwellian()
+	defer FreeMaxwellian()
+
+	p := RandParticle(Water)
+	Jump(p)
+}
+
+func TestNewJump(t *testing.T) {
+	InitMaxwellian()
+	defer FreeMaxwellian()
+
+	jump := NewJump(ButlerTemperature,RandVelocity,RandDirection,ButlerPositionJump,FlightTime)
+	p := RandParticle(Water)
+
+	jump(p)
+}
+
 func BenchmarkJump(b *testing.B) {
 	InitMaxwellian()
 	defer FreeMaxwellian()
@@ -10,5 +28,18 @@ func BenchmarkJump(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		Jump(p)
+	}
+}
+
+func BenchmarkNewJump(b *testing.B) {
+	InitMaxwellian()
+	defer FreeMaxwellian()
+	
+	jump := NewJump(ButlerTemperature,RandVelocity,RandDirection,ButlerPositionJump,FlightTime)
+
+	p := RandParticle(Water)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		jump(p)
 	}
 }
