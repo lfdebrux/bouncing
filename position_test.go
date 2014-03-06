061621036972b83ceeb2d1b081ecce91131571dd
Laurence de Bruxelles <lfdebrux@gmail.com>
2014-03-06 12:37:54 +0000

    added tests for latitude and longitude in proper range

--- position_test.go
+++ position_test.go
@@ -2,6 +2,8 @@ package bouncing
 
 import "testing"
 
+import "math"
+
 func makePosJ(phi0,beta0,v,psi,thetadash float64) *J {
 	return &J{P:&P{Phi:phi0,Beta:beta0},Velocity:v,Psi:psi,ThetaDash:thetadash}
 }
@@ -18,6 +20,57 @@ func BenchmarkVondrakPositionJump(b *testing.B) {
 	}
 }
 
+func TestButlerPositionJumpLongitude(t *testing.T) {
+	for v0 := 0.0; v0 < 2000; v0 += 1 {
+		j := makePosJ(math.Pi/2,0,v0,math.Pi/2,math.Pi/4)
+		ButlerPositionJump(j)
+		if j.Beta < 0 {
+			t.Fatal("ButlerPositionJump beta is negative")
+		}
+		if j. Beta > 2*math.Pi {
+			t.Fatal("ButlerPositionJump beta is greater than 2pi")
+		}
+	}
+}
+
+func TestButlerPositionJumpColatitude(t *testing.T) {
+	for v0 := 0.0; v0 < 2000; v0 += 1 {
+		j := makePosJ(math.Pi/2,0,v0,0,math.Pi/4)
+		ButlerPositionJump(j)
+		if j.Phi < 0 {
+			t.Fatal("ButlerPositionJump phi is negative")
+		}
+		if j. Phi > math.Pi {
+			t.Fatal("ButlerPositionJump phi is greater than pi")
+		}
+	}
+}
+
+func TestVondrakPositionJumpLongitude(t *testing.T) {
+	for v0 := 0.0; v0 < 2000; v0 += 1 {
+		j := makePosJ(math.Pi/2,0,v0,math.Pi/2,math.Pi/4)
+		VondrakPositionJump(j)
+		if j.Beta < 0 {
+			t.Fatal("VondrakPositionJump beta is negative")
+		}
+		if j. Beta > 2*math.Pi {
+			t.Fatal("VondrakPositionJump beta is greater than 2pi")
+		}
+	}
+}
+
+func TestVondrakPositionJumpColatitude(t *testing.T) {
+	for v0 := 0.0; v0 < 2000; v0 += 1 {
+		j := makePosJ(math.Pi/2,0,v0,0,math.Pi/4)
+		VondrakPositionJump(j)
+		if j.Phi < 0 {
+			t.Fatal("VondrakPositionJump phi is negative")
+		}
+		if j. Phi > math.Pi {
+			t.Fatal("VondrakPositionJump phi is greater than pi")
+		}
+	}
+}
 // func TestPositionWithLeapfrog(t *testing.T) {
 	// 
 // }
--- time_test.go
+++ time_test.go
@@ -2,8 +2,20 @@ package bouncing
 
 import "testing"
 
+import "math"
+
 func BenchmarkVondrakZenith(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		VondrakZenith(&J{P:&P{Beta:2}})
+	}
+}
+
+func TestVondrakZenithPositiveTime(t *testing.T) {
+	for beta := 0.0; beta < 2*math.Pi; beta += 0.1 {
+		j := &J{P:&P{Beta:beta,Phi:math.Pi/2}}
+		VondrakZenith(j)
+		if j.Time < 0 {
+			t.Fatalf("VondrakZenith is giving negative time to sunrise at beta=%v",beta)
+		}
 	}
 }
