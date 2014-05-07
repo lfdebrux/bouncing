cf8a9b0b5b642237bf4b3f334b1bdfb6f1e2e6d1
Laurence de Bruxelles <lfdebrux@gmail.com>
2014-05-07 21:24:27 +0100

    added killen temperature

--- temperature.go
+++ temperature.go
@@ -29,7 +29,7 @@ func VondrakTemperature(j *J) string {
 
 func KillenTemperature(j *J) {
 	if j.SolarZenith < math.Pi/2 {
-		j.Temperature = 280 * math.Pow(math.Cos(j.SolarZenith),0.25) + 100
+		j.Temperature = 300 * math.Pow(math.Cos(j.SolarZenith),0.25) + 100
 	} else if j.SolarZenith > math.Pi/2 {
 		j.Temperature = 100
 	}
@@ -39,9 +39,6 @@ func ThermalResidence(j *J) string {
 	// generate a random time to thermally accomodate before being desorped
 	rate := 1e13*math.Exp(-5802.26/j.Temperature)
 	t := -math.Log(1 - rand.Float64())/rate
-	if t > timePerDeg { // time to turn 1 degrees
-		t = timePerDeg // clamp because rotation (temperature) affects desorption rate
-	}
 	j.Time += t
 	j.Beta += 2*math.Pi*t/LunarDay // rotation
 	return fmt.Sprintln("r", j.Time, j.Phi, j.Beta, t, "thermally accomodating")
